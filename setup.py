bcefa22ad4eba10f4799ba3d2c7c8eeddeca56d0
Mikko Sipola <85122497+msipola@users.noreply.github.com>
2022-07-17 13:10:03 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md','r') as fh:
+  long_description = fh.read()
+  
+setuptools.setup(
+  name='simple_telebot',
+  version='0.1.0',
+  author='Mikko Sipola',
+  author_email='mikko.sipola@uef.fi',
+  description='Simple to use Telegram alarm bot',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/msipola/simple_telebot',
+  packages=setuptools.find_packages(),
+  install_requires=[
+    'telepot',
+    're',
+  ],
+)
